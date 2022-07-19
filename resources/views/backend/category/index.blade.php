ecd78fb39e08fc685d952cde91d9e65b0755f9fd
xuanthu <xuanthu13090@gmail.com>
2022-07-19 15:47:27 +0700

    dev7

--- resources/views/backend/category/index.blade.php
+++ resources/views/backend/category/index.blade.php
@@ -1,90 +1,87 @@
 @extends('backend.layouts.master')
 
 @section('main-content')
- <!-- DataTales Example -->
- <div class="card shadow mb-4">
-     <div class="row">
-         <div class="col-md-12">
-            @include('backend.layouts.notification')
-         </div>
-     </div>
-    <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary float-left">Category Lists</h6>
-      <a href="{{route('category.create')}}" class="btn btn-primary btn-sm float-right" data-toggle="tooltip" data-placement="bottom" title="Add User"><i class="fas fa-plus"></i> Add Category</a>
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+  <div class="row">
+    <div class="col-md-12">
+      @include('backend.layouts.notification')
     </div>
-    <div class="card-body">
-      <div class="table-responsive">
-        @if(count($categories)>0)
-        <table class="table table-bordered" id="banner-dataTable" width="100%" cellspacing="0">
-          <thead>
+  </div>
+  <div class="card-header py-3">
+    <h6 class="m-0 font-weight-bold text-primary float-left">Category Lists</h6>
+    <a href="{{route('category.create')}}" class="btn btn-primary btn-sm float-right" data-toggle="tooltip" data-placement="bottom" title="Add User"><i class="fas fa-plus"></i> Add Category</a>
+  </div>
+  <div class="card-body">
+    <div class="table-responsive">
+      @if(count($categories)>0)
+      <table class="table table-bordered" id="banner-dataTable" width="100%" cellspacing="0">
+        <thead>
+          <tr>
+            <th>**</th>
+            <th>Title</th>
+            <th>Slug</th>
+            <th>Is Parent</th>
+            <th>Parent Category</th>
+            <th>Photo</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <th>**</th>
+            <th>Title</th>
+            <th>Slug</th>
+            <th>Is Parent</th>
+            <th>Parent Category</th>
+            <th>Photo</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </tfoot>
+        <tbody>
+          @foreach($categories as $category)
+            @php
+            @endphp
             <tr>
-              <th>**</th>
-              <th>Title</th>
-              <th>Slug</th>
-              <th>Is Parent</th>
-              <th>Parent Category</th>
-              <th>Photo</th>
-              <th>Status</th>
-              <th>Action</th>
+              <td>{{$category->id}}</td>
+              <td>{{$category->title}}</td>
+              <td>{{$category->slug}}</td>
+              <td>{{(($category->is_parent==1)? 'Yes': 'No')}}</td>
+              <td>{{$category->parent_info->title ?? ''}}</td>
+              <td>
+                @if($category->photo)
+                  <img src="{{$category->photo}}" class="img-fluid" style="max-width:80px" alt="{{$category->photo}}">
+                @else
+                  <img src="{{asset('backend/img/thumbnail-default.jpg')}}" class="img-fluid" style="max-width:80px" alt="avatar.png">
+                @endif
+              </td>
+              <td>
+                @if($category->status=='active')
+                  <span class="badge badge-success">{{$category->status}}</span>
+                @else
+                  <span class="badge badge-warning">{{$category->status}}</span>
+                @endif
+              </td>
+              <td>
+                <a href="{{route('category.edit',$category->id)}}" class="btn btn-primary btn-sm float-left mr-1" style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" title="edit" data-placement="bottom"><i class="fas fa-edit"></i></a>
+                <form method="POST" action="{{route('category.destroy',[$category->id])}}">
+                  @csrf
+                  @method('delete')
+                  <button class="btn btn-danger btn-sm dltBtn" data-id={{$category->id}} style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" data-placement="bottom" title="Delete"><i class="fas fa-trash-alt"></i></button>
+                </form>
+              </td>
             </tr>
-          </thead>
-          <tfoot>
-            <tr>
-              <th>**</th>
-              <th>Title</th>
-              <th>Slug</th>
-              <th>Is Parent</th>
-              <th>Parent Category</th>
-              <th>Photo</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </tfoot>
-          <tbody>
-
-            @foreach($categories as $category)
-              @php
-              @endphp
-                <tr>
-                    <td>{{$category->id}}</td>
-                    <td>{{$category->title}}</td>
-                    <td>{{$category->slug}}</td>
-                    <td>{{(($category->is_parent==1)? 'Yes': 'No')}}</td>
-                    <td>
-                        {{$category->parent_info->title ?? ''}}
-                    </td>
-                    <td>
-                        @if($category->photo)
-                            <img src="{{$category->photo}}" class="img-fluid" style="max-width:80px" alt="{{$category->photo}}">
-                        @else
-                            <img src="{{asset('backend/img/thumbnail-default.jpg')}}" class="img-fluid" style="max-width:80px" alt="avatar.png">
-                        @endif
-                    </td>
-                    <td>
-                        @if($category->status=='active')
-                            <span class="badge badge-success">{{$category->status}}</span>
-                        @else
-                            <span class="badge badge-warning">{{$category->status}}</span>
-                        @endif
-                    </td>
-                    <td>
-                        <a href="{{route('category.edit',$category->id)}}" class="btn btn-primary btn-sm float-left mr-1" style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" title="edit" data-placement="bottom"><i class="fas fa-edit"></i></a>
-                    <form method="POST" action="{{route('category.destroy',[$category->id])}}">
-                      @csrf
-                      @method('delete')
-                          <button class="btn btn-danger btn-sm dltBtn" data-id={{$category->id}} style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" data-placement="bottom" title="Delete"><i class="fas fa-trash-alt"></i></button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-          </tbody>
-        </table>
-        <span style="float:right">{{$categories->links()}}</span>
-        @else
-          <h6 class="text-center">No Categories found!!! Please create Category</h6>
-        @endif
-      </div>
+          @endforeach
+        </tbody>
+      </table>
+      <span style="float:right">{{$categories->links()}}</span>
+      @else
+        <h6 class="text-center">No Categories found!!! Please create Category</h6>
+      @endif
     </div>
+  </div>
 </div>
 @endsection
 
@@ -108,49 +105,45 @@
   <!-- Page level custom scripts -->
   <script src="{{asset('backend/js/demo/datatables-demo.js')}}"></script>
   <script>
-
-      $('#banner-dataTable').DataTable( {
-            "columnDefs":[
-                {
-                    "orderable":false,
-                    "targets":[3,4,5]
-                }
-            ]
-        } );
-
-        // Sweet alert
-
-        function deleteData(id){
-
+    $('#banner-dataTable').DataTable( {
+      "columnDefs":[
+        {
+          "orderable":false,
+          "targets":[3,4,5]
         }
+      ]
+    } );
+    // Sweet alert
+    function deleteData(id){
+    }
   </script>
   <script>
-      $(document).ready(function(){
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
+    $(document).ready(function(){
+      $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+      });
+      $('.dltBtn').click(function(e){
+        var form=$(this).closest('form');
+        var dataID=$(this).data('id');
+        // alert(dataID);
+        e.preventDefault();
+        swal({
+          title: "Are you sure?",
+          text: "Once deleted, you will not be able to recover this data!",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        })
+        .then((willDelete) => {
+          if (willDelete) {
+            form.submit();
+          } else {
+            swal("Your data is safe!");
+          }
         });
-          $('.dltBtn').click(function(e){
-            var form=$(this).closest('form');
-              var dataID=$(this).data('id');
-              // alert(dataID);
-              e.preventDefault();
-              swal({
-                    title: "Are you sure?",
-                    text: "Once deleted, you will not be able to recover this data!",
-                    icon: "warning",
-                    buttons: true,
-                    dangerMode: true,
-                })
-                .then((willDelete) => {
-                    if (willDelete) {
-                       form.submit();
-                    } else {
-                        swal("Your data is safe!");
-                    }
-                });
-          })
       })
+    })
   </script>
 @endpush
--- resources/views/backend/coupon/create.blade.php
+++ resources/views/backend/coupon/create.blade.php
@@ -3,53 +3,50 @@
 @section('main-content')
 
 <div class="card">
-    <h5 class="card-header">Add Coupon</h5>
-    <div class="card-body">
-      <form method="post" action="{{route('coupon.store')}}">
-        {{csrf_field()}}
-        <div class="form-group">
+  <h5 class="card-header">Add Coupon</h5>
+  <div class="card-body">
+    <form method="post" action="{{route('coupon.store')}}">
+      {{csrf_field()}}
+      <div class="form-group">
         <label for="inputTitle" class="col-form-label">Coupon Code <span class="text-danger">*</span></label>
         <input id="inputTitle" type="text" name="code" placeholder="Enter Coupon Code"  value="{{old('code')}}" class="form-control">
         @error('code')
         <span class="text-danger">{{$message}}</span>
         @enderror
-        </div>
-
-        <div class="form-group">
-            <label for="type" class="col-form-label">Type <span class="text-danger">*</span></label>
-            <select name="type" class="form-control">
-                <option value="fixed">Fixed</option>
-                <option value="percent">Percent</option>
-            </select>
-            @error('type')
-            <span class="text-danger">{{$message}}</span>
-            @enderror
-        </div>
-
-        <div class="form-group">
-            <label for="inputTitle" class="col-form-label">Value <span class="text-danger">*</span></label>
-            <input id="inputTitle" type="number" name="value" placeholder="Enter Coupon value"  value="{{old('value')}}" class="form-control">
-            @error('value')
-            <span class="text-danger">{{$message}}</span>
-            @enderror
-        </div>
-        
-        <div class="form-group">
-          <label for="status" class="col-form-label">Status <span class="text-danger">*</span></label>
-          <select name="status" class="form-control">
-              <option value="active">Active</option>
-              <option value="inactive">Inactive</option>
-          </select>
-          @error('status')
-          <span class="text-danger">{{$message}}</span>
-          @enderror
-        </div>
-        <div class="form-group mb-3">
-          <button type="reset" class="btn btn-warning">Reset</button>
-           <button class="btn btn-success" type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+      </div>
+      <div class="form-group">
+        <label for="type" class="col-form-label">Type <span class="text-danger">*</span></label>
+        <select name="type" class="form-control">
+          <option value="fixed">Fixed</option>
+          <option value="percent">Percent</option>
+        </select>
+        @error('type')
+        <span class="text-danger">{{$message}}</span>
+        @enderror
+      </div>
+      <div class="form-group">
+        <label for="inputTitle" class="col-form-label">Value <span class="text-danger">*</span></label>
+        <input id="inputTitle" type="number" name="value" placeholder="Enter Coupon value"  value="{{old('value')}}" class="form-control">
+        @error('value')
+        <span class="text-danger">{{$message}}</span>
+        @enderror
+      </div>
+      <div class="form-group">
+        <label for="status" class="col-form-label">Status <span class="text-danger">*</span></label>
+        <select name="status" class="form-control">
+          <option value="active">Active</option>
+          <option value="inactive">Inactive</option>
+        </select>
+        @error('status')
+        <span class="text-danger">{{$message}}</span>
+        @enderror
+      </div>
+      <div class="form-group mb-3">
+        <button type="reset" class="btn btn-warning">Reset</button>
+        <button class="btn btn-success" type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
 </div>
 
 @endsection
@@ -61,14 +58,13 @@
 <script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
 <script src="{{asset('backend/summernote/summernote.min.js')}}"></script>
 <script>
-    $('#lfm').filemanager('image');
-
-    $(document).ready(function() {
-    $('#description').summernote({
-      placeholder: "Write short description.....",
-        tabsize: 2,
-        height: 150
-    });
-    });
+  $('#lfm').filemanager('image');
+  $(document).ready(function() {
+  $('#description').summernote({
+    placeholder: "Write short description.....",
+    tabsize: 2,
+    height: 150
+  });
+  });
 </script>
 @endpush
