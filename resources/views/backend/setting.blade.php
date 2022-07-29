8d7cad05d7a5415346294b06ded33828c2f2e1dc
xuanthu <xuanthu13090@gmail.com>
2022-07-29 10:45:42 +0700

    dev10

--- resources/views/backend/setting.blade.php
+++ resources/views/backend/setting.blade.php
@@ -31,6 +31,7 @@
               <i class="fa fa-picture-o"></i> Choose
             </a>
           </span>
+        </div>
         <input id="thumbnail1" class="form-control" type="text" name="logo" value="{{$data->logo}}">
       </div>
       <div id="holder1" style="margin-top:15px;max-height:100px;"></div>
@@ -46,6 +47,7 @@
               <i class="fa fa-picture-o"></i> Choose
             </a>
           </span>
+        </div>
         <input id="thumbnail" class="form-control" type="text" name="photo" value="{{$data->photo}}">
       </div>
       <div id="holder" style="margin-top:15px;max-height:100px;"></div>
