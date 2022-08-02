4d242aac7bfd920dae45096660a425fc9b8eb0b5
xuanthu <xuanthu13090@gmail.com>
2022-08-02 10:35:42 +0700

    dev11

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,5 @@ class DatabaseSeeder extends Seeder
          $this->call(UsersTableSeeder::class);
          $this->call(SettingTableSeeder::class);
          $this->call(CouponSeeder::class);
-
-
     }
 }
